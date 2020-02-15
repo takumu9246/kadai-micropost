aba3a53c82055a3cbd779f5a149d112af6f318a4
takumu9246 <black.diamond.9246@gmail.com>
2020-02-15 16:13:52 +0000

    for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,10 +38,15 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = current_user.favoritings.page(params[:page])
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :content)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :favoritings, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -25,5 +28,26 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end 
+  
+  def favoriting?(micropost)
+    favoritings.include?(micropost)
+  end
+  
+  def feed_likes
+    
   end
 end
